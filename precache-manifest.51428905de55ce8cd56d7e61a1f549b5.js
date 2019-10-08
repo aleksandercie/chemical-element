54e56fa2eb54c7ea523dfb069e3cf2c6f3a4645b
Aleksander <aleksander@MacBook-Air-Aleksander.local>
2019-10-08 17:48:41 +0200

    Updates

---
+++ precache-manifest.51428905de55ce8cd56d7e61a1f549b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b29d008aac0e902454443b130414652",
+    "url": "/chemical-element/index.html"
+  },
+  {
+    "revision": "ba3fdd3c2a7f0d92034f",
+    "url": "/chemical-element/static/css/main.19bed6b5.chunk.css"
+  },
+  {
+    "revision": "ab49190592a27335c859",
+    "url": "/chemical-element/static/js/2.f559598a.chunk.js"
+  },
+  {
+    "revision": "ba3fdd3c2a7f0d92034f",
+    "url": "/chemical-element/static/js/main.09f61659.chunk.js"
+  },
+  {
+    "revision": "eab37cfd5553f1bd71e3",
+    "url": "/chemical-element/static/js/runtime-main.e4a956c8.js"
+  }
+]);
